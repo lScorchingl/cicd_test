6c39bec85b6906e40690d3c72493de83436f3160
lScorchingl <gruzdkov00@mail.ru>
2025-07-25 17:16:41 +0300

    Update conftest.py
    Включил хеадлесс

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture()
 def browser():
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)
