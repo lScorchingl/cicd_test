f86f57387e279e00eb107eeaca4557e436a1f53e
lScorchingl <gruzdkov00@mail.ru>
2025-07-28 16:29:01 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+import os
 
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('--headless')
+
+    # В CI (GitHub Actions) включаем headless режим
+    if os.getenv("CI") == "true":
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)
     yield browser
+    browser.quit()
